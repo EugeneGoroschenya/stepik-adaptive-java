b2a77211d7081a8a57b7f1ed5ce05303d24be57d
Goroshchenya, Evgeny <e.goroshchenya@itransition.com>
2018-09-03 14:06:02 +0300

    https://stepik.org/lesson/44260/step/1?adaptive=true&unit=24729

---
+++ src/Task9.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Task9 {
+
+    /**
+     * https://stepik.org/lesson/44260/step/1?adaptive=true&unit=24729
+     * <p>
+     * How to get the number of processors available to the Java virtual machine?
+     */
+    public static void main(String[] args) {
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+
+}
